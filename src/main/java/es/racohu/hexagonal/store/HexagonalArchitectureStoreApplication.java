edd82063b4ecb83a01f23ee8c126cb7b87116d27
Raul Correas <raul.correas@gmail.com>
2023-09-25 22:00:32 +0200

    first version

---
+++ src/main/java/es/racohu/hexagonal/store/HexagonalArchitectureStoreApplication.java
@@ -0,0 +1,13 @@
+package es.racohu.hexagonal.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalArchitectureStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/racohu/hexagonal/store/HexagonalArchitectureStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package es.racohu.hexagonal.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
